ffed89770821bc153ef1cca3bf236b7239b98d38
CoderbabuBhagyesh <bv.coderbabu@gmail.com>
2022-07-30 09:38:22 +0530

    bug fixed

---
+++ Source code/paidwik/lib/Screen/loginScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LogingScreen extends StatefulWidget {
+  const LogingScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LogingScreen> createState() => _LogingScreenState();
+}
+
+class _LogingScreenState extends State<LogingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
