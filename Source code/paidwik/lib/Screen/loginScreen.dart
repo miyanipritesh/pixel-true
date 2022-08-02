e374aebdaac78e8b971160acb828a02646fbb8da
CoderbabuBhagyesh <bv.coderbabu@gmail.com>
2022-08-02 18:48:41 +0530

    add google map api dynamically

--- Source code/paidwik/lib/Screen/loginScreen.dart
+++ Source code/paidwik/lib/Screen/loginScreen.dart
@@ -1,15 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 
-class LogingScreen extends StatefulWidget {
-  const LogingScreen({Key? key}) : super(key: key);
+import '../Appconstant/imagefile.dart';
+import '../Widget/customButton.dart';
+import '../Widget/imageWidget.dart';
+import '../Widget/textFieldWidget.dart';
+import '../Widget/textWidget.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
-  State<LogingScreen> createState() => _LogingScreenState();
+  State<LoginScreen> createState() => _LoginScreenState();
 }
 
-class _LogingScreenState extends State<LogingScreen> {
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController userPasswordController = TextEditingController();
+  TextEditingController usernameController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: AppColor.clrBlack,
+      body: ListView(
+        children: [
+          SizedBox(
+            height: 10.h,
+          ),
+          imageWidget(ImageAssest.logo),
+          SizedBox(
+            height: 5.h,
+          ),
+          textFieldWidget('UserName', usernameController),
+          textFieldWidget('UserPassword', userPasswordController),
+          SizedBox(
+            height: 4.h,
+          ),
+          customButton(() {}, 'Sign In', Colors.deepOrange),
+          SizedBox(
+            height: 2.h,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: Row(
+                  children: [
+                    textWidget('Not a registered member?', AppColor.clrWhite),
+                    textWidget('Sign UP', AppColor.clrOrange)
+                  ],
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
--- Source code/paidwik/lib/Widget/textFieldWidget.dart
+++ Source code/paidwik/lib/Widget/textFieldWidget.dart
@@ -1,5 +1,29 @@
 import 'package:flutter/material.dart';
 
-Widget textFieldWidget() {
-  return TextFormField();
+Widget textFieldWidget(String name, TextEditingController controller) {
+  return Padding(
+    padding: const EdgeInsets.only(
+      left: 15.0,
+      right: 15,
+      bottom: 20,
+    ),
+    child: Container(
+      height: 50,
+      child: TextFormField(
+        keyboardType: TextInputType.text,
+        controller: controller,
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+            hintText: name,
+            hintStyle: TextStyle(color: Colors.white30),
+            focusColor: Colors.white54,
+            focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white12)),
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white12)),
+            border: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white12))),
+      ),
+    ),
+  );
 }
