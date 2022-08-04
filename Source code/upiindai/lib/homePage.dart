8d8e3e3e764b22fa631055d20d1c25e0d2383d46
CoderbabuBhagyesh <bv.coderbabu@gmail.com>
2022-08-04 14:39:55 +0530

    add project

--- Source code/upiindai/lib/homePage.dart
+++ Source code/upiindai/lib/homePage.dart
@@ -12,35 +12,36 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   Future getData() async {
     final responce = await http.get(Uri.parse(
-        'https://www.bot.or.th/Thai/ConsumerInfo/Fraud/Pages/BOTLicensedLoan.aspx'));
+        'https://www.cricketexchange.in/scoreboard/F95/13W/Final/EY/3Z/sgt-vs-bd-final-shpageeza-cricket-league-2022/live'));
 
     dom.Document html = dom.Document.html(responce.body);
 
     final title = html
-        .getElementsByClassName(
-            'bg-gray grpctl73_g_e8579_0 grpctl73_g_e8579_0 grpctl73_g_e8579_0')
-        .map((e) => e.getElementsByTagName('a')[0].attributes['href'])
+        .getElementsByClassName('live-score-card')
+        .map((e) =>
+            e.children[0].children[0].children[1].children[1].children[0].text)
         .toList();
 
-    final logo = html
-        .getElementsByClassName(
-            'bg-gray grpctl73_g_e8579_0 grpctl73_g_e8579_0 grpctl73_g_e8579_0')
-        .map((e) => e.getElementsByTagName('img')[0].attributes['src'])
-        .toList();
-
-    final text = html
-        .getElementsByClassName(
-            'bg-gray grpctl73_g_e8579_0 grpctl73_g_e8579_0 grpctl73_g_e8579_0')
-        .map((e) {
-      print('++++++++++++++++${e}');
-      return e.getElementsByTagName('a')[0].attributes['href'];
-    }).toList();
+    // final logo = html
+    //     .getElementsByClassName(
+    //         'bg-gray grpctl73_g_e8579_0 grpctl73_g_e8579_0 grpctl73_g_e8579_0')
+    //     .map((e) => e.getElementsByTagName('img')[0].attributes['src'])
+    //     .toList();
+    //
+    // final text = html
+    //     .getElementsByClassName(
+    //         'bg-gray grpctl73_g_e8579_0 grpctl73_g_e8579_0 grpctl73_g_e8579_0')
+    //     .map((e) {
+    //   print(
+    //       '++++++++++++++++${e.children[0].getElementsByTagName('img')[0].attributes['src']}');
+    //   return e.getElementsByTagName('a')[0].attributes['href'];
+    // }).toList();
 
     print('*****************$title');
 
-    print('*****************$logo');
-
-    print('*****************$text');
+    // print('*****************$logo');
+    //
+    // print('*****************$text');
   }
 
   @override
