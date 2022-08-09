f44662b2dc64bd2f276ebb1fd8909a678bf14935
CoderbabuBhagyesh <bv.coderbabu@gmail.com>
2022-08-09 18:49:47 +0530

    working on web scrapping

---
+++ Flutter Code/build/ios_insecure_screen_detector/generated/source/buildConfig/debug/com/staking_power/ios_insecure_screen_detector/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.staking_power.ios_insecure_screen_detector;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.staking_power.ios_insecure_screen_detector";
+  public static final String BUILD_TYPE = "debug";
+}
